23a81789f797b7d455e07099d4c51f471ba4fcaa
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-06-05 22:23:18 +0200

    feat: ✨ chat listeners and methods

---
+++ src/system/chat.js
@@ -0,0 +1,54 @@
+import BRRollHandler from 'src/components/roll/roller.js';
+
+/**
+ * Adds Event Listeners to the Chat log.
+ * @param {JQuery} html
+ */
+export function addChatListeners(html) {
+  html.find('.dice-button.push').on('click', _onPushRoll);
+}
+
+/* ------------------------------------------- */
+/*  Roll Push                                  */
+/* ------------------------------------------- */
+
+/**
+ * Triggers a push from the chat.
+ * @param {Event} event
+ * @returns {Promise<import('@lib/yzur').YearZeroRoll|ChatMessage>}
+ */
+function _onPushRoll(event) {
+  event.preventDefault();
+
+  // Disables the button to avoid any tricky double push.
+  const button = event.currentTarget;
+  button.disabled = true;
+
+  // Gets infos and requires a push.
+  const chatCard = event.currentTarget.closest('.chat-message');
+  const messageId = chatCard.dataset.messageId;
+  const message = game.messages.get(messageId);
+  return BRRollHandler.pushRoll(message);
+}
+
+/* ------------------------------------------- */
+/*  Hiding Buttons                             */
+/* ------------------------------------------- */
+
+/**
+ * Hides buttons of Chat messages for non-owners.
+ * @param {JQuery} html
+ */
+export function hideChatActionButtons(html) {
+  const chatCard = html.find('.blade-runner.chat-card');
+
+  // Exits early if no chatCard were found.
+  if (chatCard.length === 0) return;
+
+  // Hides buttons.
+  const actor = game.actors.get(chatCard.attr('data-actor-id'));
+  const buttons = chatCard.find('button');
+  for (const btn of buttons) {
+    if (actor && !actor.isOwner) btn.style.display = 'none';
+  }
+}
