44b2d45adac63537efbc81edd8c9632abb591504
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-06-12 23:59:42 +0200

    feat(DsN): ✨ Dice-So-Nice templates

--- src/plugins/dice-so-nice.js
+++ src/plugins/dice-so-nice.js
@@ -8,12 +8,80 @@ export function registerDiceSoNice(dice3d) {
     name: 'flbr-base',
     category: 'Blade Runner RPG',
     description: 'Blade Runner Base Die',
-    foreground: '#f00',
-    background: '#000',
+    foreground: '#fff',
+    background: '#C53426',
     outline: 'none',
     // edge: '#000',
     texture: 'none',
     material: 'metal',
     font: 'Consolas',
   }, 'default');
+
+  dice3d.addDicePreset({
+    type: 'd6',
+    labels: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+    ],
+    system: 'blade-runner',
+    colorset: 'flbr-base',
+  }, 'd6');
+
+  dice3d.addDicePreset({
+    type: 'd8',
+    labels: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+    ],
+    system: 'blade-runner',
+    colorset: 'flbr-base',
+  }, 'd8');
+
+  dice3d.addDicePreset({
+    type: 'd10',
+    labels: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+    ],
+    system: 'blade-runner',
+    colorset: 'flbr-base',
+  }, 'd10');
+
+  dice3d.addDicePreset({
+    type: 'd12',
+    labels: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+    ],
+    system: 'blade-runner',
+    colorset: 'flbr-base',
+  }, 'd12');
 }
