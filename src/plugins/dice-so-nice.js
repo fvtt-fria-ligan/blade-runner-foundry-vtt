ae1448906e5c5ce9fd5239a1150b8789cd154745
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-10-15 14:54:09 +0200

    feat(DsN): ✨ Add dice icons for Dice So Nice

--- src/plugins/dice-so-nice.js
+++ src/plugins/dice-so-nice.js
@@ -5,7 +5,7 @@ export function registerDiceSoNice(dice3d) {
   }, 'preferred');
 
   dice3d.addColorset({
-    name: 'flbr-base',
+    name: 'blade-runner-base-die',
     category: 'Blade Runner RPG',
     description: 'Blade Runner Base Die',
     foreground: '#fff',
@@ -14,74 +14,106 @@ export function registerDiceSoNice(dice3d) {
     // edge: '#000',
     texture: 'none',
     material: 'metal',
-    font: 'Consolas',
+    font: 'IdentikalSans, Consolas, sans-Serif',
   }, 'default');
 
   dice3d.addDicePreset({
     type: 'd6',
     labels: [
-      'systems/blade-runner/assets/icons/blade-runner-failure.webp',
+      'systems/blade-runner/assets/dice/d6/br_d6_1.webp',
       '2',
       '3',
       '4',
       '5',
-      '6',
+      'systems/blade-runner/assets/dice/d6/br_d6_6.webp',
+    ],
+    // eslint-disable-next-line no-sparse-arrays
+    bumpMaps: [
+      'systems/blade-runner/assets/dice/d6/br_d6_1.webp',,,,,
+      'systems/blade-runner/assets/dice/d6/br_d6_6.webp',
     ],
     system: 'blade-runner',
-    colorset: 'flbr-base',
+    colorset: 'blade-runner-base-die',
   }, 'd6');
 
   dice3d.addDicePreset({
     type: 'd8',
     labels: [
-      'systems/blade-runner/assets/icons/blade-runner-failure.webp',
+      'systems/blade-runner/assets/dice/d8/br_d8_1.webp',
       '2',
       '3',
       '4',
       '5',
-      '6',
-      '7',
-      '8',
+      'systems/blade-runner/assets/dice/d8/br_d8_6.webp',
+      'systems/blade-runner/assets/dice/d8/br_d8_7.webp',
+      'systems/blade-runner/assets/dice/d8/br_d8_8.webp',
+    ],
+    // eslint-disable-next-line no-sparse-arrays
+    bumpMaps: [
+      'systems/blade-runner/assets/dice/d8/br_d8_1_bump.webp',,,,,
+      'systems/blade-runner/assets/dice/d8/br_d8_6_bump.webp',
+      'systems/blade-runner/assets/dice/d8/br_d8_7_bump.webp',
+      'systems/blade-runner/assets/dice/d8/br_d8_8_bump.webp',
     ],
     system: 'blade-runner',
-    colorset: 'flbr-base',
+    colorset: 'blade-runner-base-die',
   }, 'd8');
 
   dice3d.addDicePreset({
     type: 'd10',
     labels: [
-      'systems/blade-runner/assets/icons/blade-runner-failure.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_1.webp',
       '2',
       '3',
       '4',
       '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '10',
+      'systems/blade-runner/assets/dice/d10/br_d10_6.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_7.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_8.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_9.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_10.webp',
+    ],
+    // eslint-disable-next-line no-sparse-arrays
+    bumpMaps: [
+      'systems/blade-runner/assets/dice/d10/br_d10_1_bump.webp',,,,,
+      'systems/blade-runner/assets/dice/d10/br_d10_6_bump.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_7_bump.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_8_bump.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_9_bump.webp',
+      'systems/blade-runner/assets/dice/d10/br_d10_10_bump.webp',
     ],
     system: 'blade-runner',
-    colorset: 'flbr-base',
+    colorset: 'blade-runner-base-die',
   }, 'd10');
 
   dice3d.addDicePreset({
     type: 'd12',
     labels: [
-      'systems/blade-runner/assets/icons/blade-runner-failure.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_1.webp',
       '2',
       '3',
       '4',
       '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '10',
-      '11',
-      '12',
+      'systems/blade-runner/assets/dice/d12/br_d12_6.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_7.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_8.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_9.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_10.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_11.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_12.webp',
+    ],
+    // eslint-disable-next-line no-sparse-arrays
+    bumpMaps: [
+      'systems/blade-runner/assets/dice/d12/br_d12_1_bump.webp',,,,,
+      'systems/blade-runner/assets/dice/d12/br_d12_6_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_7_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_8_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_9_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_10_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_11_bump.webp',
+      'systems/blade-runner/assets/dice/d12/br_d12_12_bump.webp',
     ],
     system: 'blade-runner',
-    colorset: 'flbr-base',
+    colorset: 'blade-runner-base-die',
   }, 'd12');
 }
