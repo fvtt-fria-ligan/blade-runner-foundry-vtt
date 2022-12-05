a838cd952d0421abae299373a6799e9753be01f1
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-12-05 15:52:45 +0100

    feat: ✨ Add Loot (item pile) sheet

--- src/system/sheets.js
+++ src/system/sheets.js
@@ -1,4 +1,5 @@
 import BladeRunnerCharacterSheet from '@actor/character/character-sheet.js';
+import BladeRunnerLootSheet from '@actor/loot/loot-sheet.js';
 import BladeRunnerItemSheet from '@item/item-sheet.js';
 import { ACTOR_TYPES, SYSTEM_ID, ITEM_TYPES } from './constants.js';
 
@@ -8,6 +9,7 @@ export function registerSheets() {
   Actors.unregisterSheet('core', ActorSheet);
   Actors.registerSheet(br, BladeRunnerCharacterSheet, { types: [ACTOR_TYPES.CHAR], makeDefault: true });
   // Actors.registerSheet(br, undefined, { types: [ACTOR_TYPES.VEHICLE], makeDefault: true });
+  Actors.registerSheet(br, BladeRunnerLootSheet, { types: [ACTOR_TYPES.LOOT], makeDefault: true });
 
   Items.unregisterSheet('core', ItemSheet);
   Items.registerSheet(br, BladeRunnerItemSheet, { types: [ITEM_TYPES.GENERIC], makeDefault: true });
