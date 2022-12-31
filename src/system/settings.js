52cd5aaf5526511b6fa1b731524d9a639ba16d81
Stefouch <stefouch@gmail.com>
2022-12-31 10:25:16 +0100

    fix: 🐛 settings

--- src/system/settings.js
+++ src/system/settings.js
@@ -24,6 +24,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.CRUSHING_TABLE, {
     name: 'SETTINGS.BLADE_RUNNER.CrushingTableName',
     hint: 'SETTINGS.BLADE_RUNNER.CrushingTableHint',
+    scope: 'world',
     config: true,
     type: String,
     default: '',
@@ -31,6 +32,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.PIERCING_TABLE, {
     name: 'SETTINGS.BLADE_RUNNER.PiercingTableName',
     hint: 'SETTINGS.BLADE_RUNNER.PiercingTableHint',
+    scope: 'world',
     config: true,
     type: String,
     default: '',
@@ -38,6 +40,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.EDIT_NATURE_PERMISSION, {
     name: 'SETTINGS.BLADE_RUNNER.EditNaturePermissionName',
     hint: 'SETTINGS.BLADE_RUNNER.EditNaturePermissionHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: false,
@@ -45,6 +48,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.AUTO_APPLY_DAMAGE, {
     name: 'SETTINGS.BLADE_RUNNER.AutomaticApplyDamageName',
     hint: 'SETTINGS.BLADE_RUNNER.AutomaticApplyDamageHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: true,
@@ -52,6 +56,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.AUTO_ARMOR_ROLL, {
     name: 'SETTINGS.BLADE_RUNNER.AutomaticArmorRollName',
     hint: 'SETTINGS.BLADE_RUNNER.AutomaticArmorRollHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: false,
@@ -59,6 +64,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.OPEN_FIRST_WEAPON_ATTACK, {
     name: 'SETTINGS.BLADE_RUNNER.OpenFirstWeaponAttackName',
     hint: 'SETTINGS.BLADE_RUNNER.OpenFirstWeaponAttackHint',
+    scope: 'client',
     config: true,
     type: Boolean,
     default: true,
@@ -66,6 +72,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.UPDATE_ACTOR_MANEUVERABILITY_ON_CREW, {
     name: 'SETTINGS.BLADE_RUNNER.UpdateActorManeuverabilityOnCrewName',
     hint: 'SETTINGS.BLADE_RUNNER.UpdateActorManeuverabilityOnCrewHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: true,
@@ -73,6 +80,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.UPDATE_ACTOR_MANEUVERABILITY_ON_UNCREW, {
     name: 'SETTINGS.BLADE_RUNNER.UpdateActorManeuverabilityOnUncrewName',
     hint: 'SETTINGS.BLADE_RUNNER.UpdateActorManeuverabilityOnUncrewHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: false,
@@ -80,6 +88,7 @@ export function registerSystemSettings() {
   game.settings.register(sysId, SETTINGS_KEYS.USE_ACTIVE_EFFECTS, {
     name: 'SETTINGS.BLADE_RUNNER.UseActiveEffectsName',
     hint: 'SETTINGS.BLADE_RUNNER.UseActiveEffectsHint',
+    scope: 'world',
     config: true,
     type: Boolean,
     default: false,
