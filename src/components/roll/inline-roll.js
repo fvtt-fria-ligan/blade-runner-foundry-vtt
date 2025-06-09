15381ce0de99cde2da474bccde990a4f720717c7
Stefouch <stefouch@gmail.com>
2025-06-09 22:48:19 +0200

    fix: 🐛 Attempt to repair the a.inline-roll overriding body event listener

--- src/components/roll/inline-roll.js
+++ src/components/roll/inline-roll.js
@@ -2,38 +2,47 @@ import { ATTRIBUTES, SKILLS } from '@system/constants';
 import { getActiveActor } from '@utils/get-actor';
 
 export function overrideInlineRollListener() {
-  const body = $('body');
-  body.off('click', 'a.inline-roll'); // Disables Foundry listener.
-  body.on('click', 'a.inline-roll', async event => {
-    event.preventDefault();
-    const a = event.currentTarget;
-    const formula = a.dataset.formula;
+  // ! Not working, cannot disable since Foundry removed jQuery from their code.
+  // ! document.body.removeEventListener('click', null); // Disables Foundry listener.
+  // Adds our own custom body click event listener.
+  document.body.addEventListener('click', async event => {
+    // if (event.target?.closest('a[data-link]')) {
+    //   // ! Copy of method TextEditor.#onClickContentLink(event) :
+    //   event.preventDefault();
+    //   const doc = await foundry.utils.fromUuid(event.target.closest('a[data-link]').dataset.uuid);
+    //   return doc?._onClickDocumentLink(event);
+    // }
+    if (event.target?.closest('a.inline-roll')) {
+      event.preventDefault();
+      const a = event.target;
+      const formula = a.dataset.formula;
 
-    if (/np|p(?:\d+|@maxPush)/i.test(formula)) {
-      const actor = await getActiveActor();
-      const roll = Roll.create(formula, actor?.getRollData());
-      await roll.roll();
+      if (/np|p(?:\d+|@maxPush)/i.test(formula)) {
+        const actor = await getActiveActor();
+        const roll = game.bladerunner.roll.create(formula, actor?.getRollData());
+        await roll.roll();
 
-      const dice = roll.terms
-        .filter(t => t instanceof DiceTerm)
-        .flatMap(t => new Array(t.number).fill(t.faces));
+        const dice = roll.terms
+          .filter(t => t instanceof foundry.dice.terms.DiceTerm)
+          .flatMap(t => new Array(t.number).fill(t.faces));
 
-      const { attributeKey, skillKey } = _getAttributeAndSkillKeys(formula);
+        const { attributeKey, skillKey } = _getAttributeAndSkillKeys(formula);
 
-      return game.bladerunner.roller.create({
-        title: _createTitle(formula, a.dataset.flavor, actor, attributeKey, skillKey),
-        actor,
-        dice,
-        attributeKey,
-        skillKey,
-        modifiers: actor ? actor.getRollModifiers({ targets: [attributeKey, skillKey] }) : [],
-        maxPush: roll.maxPush,
-      }, {
-        rollMode: a.dataset.mode,
-      });
-    }
-    else {
-      return foundry.applications.ux.TextEditor._onClickInlineRoll(event);
+        return game.bladerunner.roller.create({
+          title: _createTitle(formula, a.dataset.flavor, actor, attributeKey, skillKey),
+          actor,
+          dice,
+          attributeKey,
+          skillKey,
+          modifiers: actor ? actor.getRollModifiers({ targets: [attributeKey, skillKey] }) : [],
+          maxPush: roll.maxPush,
+        }, {
+          rollMode: a.dataset.mode,
+        });
+      }
+      else {
+        return foundry.applications.ux.TextEditor._onClickInlineRoll(event);
+      }
     }
   });
   console.log('Blade Runner RPG | Inline-roll listener overridden.');
