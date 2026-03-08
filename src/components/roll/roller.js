975a2dfa8a2a61a8d836d9799265ec0fa5822a15
Cussa Mitre <cussa.mitre@gmail.com>
2026-03-08 14:26:20 +0000

    feat: ✨ improvements on the critical injury handler
    - Fixes #68

--- src/components/roll/roller.js
+++ src/components/roll/roller.js
@@ -558,7 +558,7 @@ export default class BRRollHandler extends FormApplication {
    * @param {YearZeroRoll} roll
    */
   static async applyCrit(roll) {
-    let actor = await getActiveActor();
+    let actor = await getActiveActor(true);
     if (!actor && game.user.isGM) {
       const actors = game.actors.filter(a => a.type === ACTOR_TYPES.CHAR || a.isVehicle);
       actor = await chooseActor(actors, {
